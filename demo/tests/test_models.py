3c4b807b93d12d94d54cfaba1e7fc4a2b3244e49
SaadKzs <Saadzebiri65@gmail.com>
2025-11-12 15:31:14 +0100

    [FIX] test corrige test_models.py dans description

--- demo/tests/test_models.py
+++ demo/tests/test_models.py
@@ -29,7 +29,7 @@ class TestUsers(TransactionCase):
                     'name': 'Marie-Noël',
                     'login': 'mnv',
                 })
-                user.description = None
+                user.description = 'test passed'
 
 
     def test_description_one_line(self):
